ef1585d352fd211c68c57c8f56a2ed0397d918b6
pigooosuke <yusuke.saito@binc.jp>
2021-12-30 15:11:56 +0900

    fix day5

--- day5-2/main.py
+++ day5-2/main.py
@@ -7,29 +7,39 @@ class Vents():
         y_moment = True
         if from_x > to_x:
             from_x, to_x = to_x, from_x
-        if from_y > to_y:
             from_y, to_y = to_y, from_y
+        if from_y > to_y:
             y_moment = False
         # draw x-axis
         if from_y == to_y:
-            for i in range(from_x, to_x + 1, 1):
-                self.areas[i][from_y] += 1
+            delta = max(abs(from_x - to_x), abs(from_y - to_y))
+            for i in range(delta + 1):
+                if y_moment is True:
+                    x = from_x + i
+                else:
+                    x = from_x - i
+                y = from_y
+                self.areas[x][y] += 1
         # draw y-axis
         elif from_x == to_x:
-            for i in range(from_y, to_y + 1, 1):
-                self.areas[from_x][i] += 1
+            delta = max(abs(from_x - to_x), abs(from_y - to_y))
+            for i in range(delta + 1):
+                if y_moment is True:
+                    y = from_y + i
+                else:
+                    y = from_y - i
+                x = from_x
+                self.areas[x][y] += 1
         # draw diag
         else:
-            if y_moment is True:
-                delta = abs(from_x - to_x)
-                print(from_x, from_y, to_x, to_y, delta, y_moment)
-                for i in range(delta):
-                    self.areas[from_x + i][from_y + i] += 1
-            else:
-                delta = abs(from_x - to_x)
-                print(from_x, from_y, to_x, to_y, delta, y_moment)
-                for i in range(delta):
-                    self.areas[from_x + i][from_y - i] += 1
+            delta = max(abs(from_x - to_x), abs(from_y - to_y))
+            for i in range(delta + 1):
+                if y_moment is True:
+                    y = from_y + i
+                else:
+                    y = from_y - i
+                x = from_x + i
+                self.areas[x][y] += 1
 
     def calc_score(self):
         score = 0
@@ -50,7 +60,8 @@ if __name__ == "__main__":
             to_x, to_y = list(map(int, to_pos.split(",")))
             max_size = max([from_x, from_y, to_x, to_y, max_size])
             inputs.append([from_x, from_y, to_x, to_y])
-    vents = Vents(max_size)
+    vents = Vents(max_size + 1)
+    print(max_size)
     for d in inputs:
         from_x, from_y, to_x, to_y = d
         vents.draw_line(from_x, from_y, to_x, to_y)
